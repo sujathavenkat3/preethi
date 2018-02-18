11e0c9fe712e586c0eec85b1fe61add86c3f2d51
sujathavenkat3 <36602781+sujathavenkat3@users.noreply.github.com>
2018-02-18 23:04:19 +0530

    num.c

---
+++ num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a>0)
+	printf("the number is positive");
+	else if(a<0)
+	printf("the num is negative");
+	else
+	printf("num is zero");
+	return 0;
+}
+
+
